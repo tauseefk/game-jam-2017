cb11c01c88467fd0c2b52abe62726154fcb1c043
Jibran Khan <jibrankhan00@hotmail.com>
2017-01-21 13:44:57 -0800

    Added neuron and glow plugin
    Added neuron and glow plugin

---
+++ Assets/Scripts/Constants.cs
@@ -0,0 +1,8 @@
+﻿public static class Constants
+{
+    public static string MK_GLOW_TEX = "_MKGlowTex";
+    public static string MK_GLOW_TEX_STR = "_MKGlowTexStrength";
+    public static string MK_GLOW_COL = "_MKGlowColor";
+    public static string MK_GLOW_TEX_COL = "_MKGlowTexColor";
+    public static string MK_GLOW_POW = "_MKGlowPower";
+}
---
+++ Assets/Scripts/Neuron.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Neuron : MonoBehaviour {
+
+    // Reference to renderer
+    MeshRenderer mRender;
+
+    // Material to apply at start
+    [SerializeField]
+    Material material;
+
+    // Initial texture size
+    [SerializeField]
+    Vector2 initTextureSize;
+
+	// Use this for initialization
+	void Start () {
+
+        // Setup
+        mRender = GetComponentInChildren<MeshRenderer>();
+        mRender.material = new Material(material);
+
+        // Scale texture on MK Shader
+        mRender.material.mainTextureScale = initTextureSize;
+        mRender.material.SetTextureScale(Constants.MK_GLOW_TEX, initTextureSize);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SetMKGlowTexture(Texture texture)
+    {
+        mRender.material.SetTexture(Constants.MK_GLOW_TEX, texture);
+    }
+
+    public void SetMKGlowColor(Color color)
+    {
+        mRender.material.SetColor(Constants.MK_GLOW_COL, color);
+    }
+
+    public void SetMKGlowTextureColor(Color color)
+    {
+        mRender.material.SetColor(Constants.MK_GLOW_TEX_COL, color);
+    }
+
+    public void SetMKGlowPower(float power)
+    {
+        mRender.material.SetFloat(Constants.MK_GLOW_POW, power);
+    }
+
+    public void SetMKGlowTextureStrength(float glowTexStr)
+    {
+        mRender.material.SetFloat(Constants.MK_GLOW_TEX_STR, glowTexStr);
+    }
+}
