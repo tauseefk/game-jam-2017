0f9e0562de71d9825c11054ad2f0e74d30efe260
Jibran Khan <jibrankhan00@hotmail.com>
2017-01-22 05:05:02 -0800

    End Game Scene
    Added end game scene with logic based on who won the game, will show
    different movie

---
+++ Assets/Scripts/EndScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndScript : MonoBehaviour {
+
+    [SerializeField]
+    GameObject good, bad;
+
+    RawImage rim;
+    MovieTexture mt;
+
+	// Use this for initialization
+	void Start () {
+
+        // Good won
+        if (GameState.angelScore > GameState.devilScore)
+        {
+            good.SetActive(true);
+            rim = good.GetComponent<RawImage>();
+        }
+        else
+        {
+            bad.SetActive(true);
+            rim = bad.GetComponent<RawImage>();
+        }
+
+        mt = (MovieTexture)rim.mainTexture;
+        mt.Play();
+    }
+}
