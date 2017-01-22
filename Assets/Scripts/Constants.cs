372541544a07bed42508cba2a29091cc35539a81
Jibran Khan <jibrankhan00@hotmail.com>
2017-01-21 18:03:27 -0800

    Added new scene, new constants, changes to main scene
    Added new scene, new constants, changes to main scene

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -5,4 +5,15 @@
     public static string MK_GLOW_COL = "_MKGlowColor";
     public static string MK_GLOW_TEX_COL = "_MKGlowTexColor";
     public static string MK_GLOW_POW = "_MKGlowPower";
+
+    // GAME STATES
+    public static int GAME_STATE;
+    public static int GAME_START = 0;
+    public static int PLAYER_1_START = 1;
+    public static int PLAYER_1_END = 2;
+    public static int PLAYER_2_START = 3;
+    public static int PLAYER_2_END = 4;
+    public static int GAME_END = 5;
+    public static int GOOD_END = 6;
+    public static int BAD_END = 7;
 }
