754af27dc402a4d938953be85c6ca92139f8ca52
Jibran Khan <jibrankhan00@hotmail.com>
2017-01-21 19:33:51 -0800

    Added angel and devil
    Added angel and devil

---
+++ Assets/Angel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Angel : MonoBehaviour {
+
+    Animator animator;
+
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            animator.SetBool("Click", true);
+        }
+	}
+
+    void AnimationEnd()
+    {
+        animator.SetBool("Click", false);
+    }
+}
---
+++ Assets/Devil.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Devil : MonoBehaviour {
+
+    Animator animator;
+
+    // Use this for initialization
+    void Start () {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            animator.SetBool("Click", true);
+        }
+    }
+
+    public void AnimationEnd()
+    {
+        print("HELLO!");
+        animator.SetBool("Click", false);
+    }
+}
