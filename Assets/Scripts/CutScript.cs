88aa9bf0b75831c460d3e3d01519c3766b048f80
Jibran Khan <jibrankhan00@hotmail.com>
2017-01-21 18:14:28 -0800

    Added Start/End Screen
    Added Start/End Screen

---
+++ Assets/Scripts/CutScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CutScript : MonoBehaviour {
+
+    [SerializeField]
+    Image image;
+
+    AudioSource audioSrc;
+
+	// Use this for initialization
+	void Start () {
+        audioSrc = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
