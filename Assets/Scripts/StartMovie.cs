fc13ff6f1c06f7b3d89eb8d474bfa4a4d916ed7f
Jibran Khan <jibrankhan00@hotmail.com>
2017-01-22 03:56:36 -0800

    Added Start Scene
    Added Start Scene with video and music

---
+++ Assets/Scripts/StartMovie.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartMovie : MonoBehaviour {
+
+    MovieTexture mt;
+    AudioSource audioSrc;
+
+    float timeLeft = 11.0f;
+
+	// Use this for initialization
+	void Start () {
+
+        RawImage rim = GetComponent<RawImage>();
+        audioSrc = GetComponent<AudioSource>();
+        
+        mt = (MovieTexture)rim.mainTexture;
+
+        audioSrc.clip = mt.audioClip;
+
+        audioSrc.Play();
+        mt.Play();
+    }
+
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        if (timeLeft < 0)
+        {
+            audioSrc.Stop();
+        }
+    }
+}
