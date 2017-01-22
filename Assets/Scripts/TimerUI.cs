01951472c35ef8977b6c53fa6e7a728240c127dd
Mohammed Tauseef <tauseefk@github.com>
2017-01-21 22:00:29 -0500

    timer prefabs and scene transition utilities

---
+++ Assets/Scripts/TimerUI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerUI : MonoBehaviour {
+
+	[SerializeField]
+	private Text _timerLabel;
+
+	[SerializeField]
+	private float _timerLength = 30F;
+
+	private float _startTime;
+
+	private float _timeRemaining;
+
+	void Start() {
+		_startTime = Time.time;
+		_timeRemaining = _timerLength;
+	}
+
+	void Update() {
+		Debug.Log (_timerLength);
+		Debug.Log(Time.time - _startTime);
+		_timeRemaining = _timerLength - Mathf.Floor(Time.time - _startTime);
+
+		//update the label value
+		_timerLabel.text = string.Format("{0:00} : {1:00}", "00", _timeRemaining);
+	}
+}
