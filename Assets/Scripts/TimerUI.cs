fa733c9451c3f97eb7549a966d340e9696b89bef
Mohammed Tauseef <tauseefk@github.com>
2017-01-21 22:40:14 -0500

    scene transition canvas and timer UI finished

--- Assets/Scripts/TimerUI.cs
+++ Assets/Scripts/TimerUI.cs
@@ -9,6 +9,9 @@ public class TimerUI : MonoBehaviour {
 	private Text _timerLabel;
 
 	[SerializeField]
+	private GameObject _transitionCanvas;
+
+	[SerializeField]
 	private float _timerLength = 30F;
 
 	private float _startTime;
@@ -21,10 +24,11 @@ public class TimerUI : MonoBehaviour {
 	}
 
 	void Update() {
-		Debug.Log (_timerLength);
-		Debug.Log(Time.time - _startTime);
-		_timeRemaining = _timerLength - Mathf.Floor(Time.time - _startTime);
-
+		if (_timeRemaining > 0) {
+			_timeRemaining = _timerLength - Mathf.Floor (Time.time - _startTime);
+		} else {
+			_transitionCanvas.SetActive (true);
+		}
 		//update the label value
 		_timerLabel.text = string.Format("{0:00} : {1:00}", "00", _timeRemaining);
 	}
